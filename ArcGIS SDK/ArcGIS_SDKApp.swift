af8ed1dc32776aede44181dc02bb0befd38178e3
fadhli <fadliprestigious@gmail.com>
2024-05-20 10:48:38 +0800

    Initial Commit

---
+++ ArcGIS SDK/ArcGIS_SDKApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArcGIS_SDKApp.swift
+//  ArcGIS SDK
+//
+//  Created by Fadhli Firdaus on 20/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct ArcGIS_SDKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
